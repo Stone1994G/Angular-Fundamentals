8259d2ecd2fd6e9de52f097e5f72fb00d9dea457
Stone1994G <45983286+Stone1994G@users.noreply.github.com>
2020-07-26 10:07:06 -0700

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+import { CoursesComponent} from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import Components here from other angular files
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
